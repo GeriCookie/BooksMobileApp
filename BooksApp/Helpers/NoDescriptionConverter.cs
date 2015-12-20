9bff1b6072d49bfdc3badecc5b1210579e7c36e0
GeriCookie <gergana.bailova@gmail.com>
2015-12-21 01:54:49 +0200

    Updated the UI of the Books

---
+++ BooksApp/Helpers/NoDescriptionConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace BooksApp.Helpers
+{
+  using System;
+  using Windows.UI.Xaml.Data;
+
+  public class NoDescriptionConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+      if (targetType != typeof(string))
+      {
+        return null;
+      }
+
+      var description = value.ToString().Trim();
+      if (string.IsNullOrEmpty(description))
+      {
+        return "No description";
+      }
+
+      return description.Substring(0, Math.Min(400, description.Length));
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ BooksApp/Helpers/NoRatingConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace BooksApp.Helpers
+{
+  using System;
+  using Windows.UI.Xaml.Data;
+
+  public class NoRatingConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+      if (targetType != typeof(string))
+      {
+        return null;
+      }
+      var rating = (int?)value;
+
+      if (!rating.HasValue)
+      {
+        return "No rating";
+      }
+
+      return rating.Value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
