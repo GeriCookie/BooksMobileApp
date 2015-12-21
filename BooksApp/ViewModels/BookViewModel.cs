d66a97e9b9977b0afe8eb81b2cc1af84e32baf14
GeriCookie <gergana.bailova@gmail.com>
2015-12-21 02:00:24 +0200

    Added description for the BookViewModel

--- BooksApp/ViewModels/BookViewModel.cs
+++ BooksApp/ViewModels/BookViewModel.cs
@@ -7,26 +7,29 @@ using System.Threading.Tasks;
 
 namespace BooksApp.ViewModels
 {
-    public class BookViewModel:ViewModelBase
-    {
-        public string Author { get; private set; }
-        public string CoverUrl { get; private set; }
-        public string Id { get; private set; }
-        public int? Pages { get; private set; }
-        public int? Rating { get; private set; }
-        public string Title { get; private set; }
+  public class BookViewModel : ViewModelBase
+  {
+    public string Author { get; private set; }
+    public string CoverUrl { get; private set; }
+    public string Id { get; private set; }
+    public int? Pages { get; private set; }
+    public int? Rating { get; private set; }
+    public string Title { get; private set; }
 
-        public static BookViewModel FromModel(BookModel model)
-        {
-            return new BookViewModel()
-            {
-                Id = model.Id,
-                Title = model.Title,
-                Author = model.Author,
-                Rating = model.Rating,
-                CoverUrl = model.CoverUrl,
-                Pages = model.Pages
-            };
-        }
+    public string Description { get; set; }
+
+    public static BookViewModel FromModel(BookModel model)
+    {
+      return new BookViewModel()
+      {
+        Id = model.Id,
+        Title = model.Title,
+        Author = model.Author,
+        Rating = model.Rating,
+        CoverUrl = model.CoverUrl,
+        Pages = model.Pages,
+        Description = model.Description
+      };
     }
+  }
 }
